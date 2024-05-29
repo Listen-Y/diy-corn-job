c319e993c224a2915edcc68c74d0b1ef1bd738cc
lixingya.123 <lixingya.123@bytedance.com>
2024-05-29 13:06:35 +0800

    feat:first demo
    Change-Id: I85a450c983a742adb616a80f7aad4964f8d53071

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/diy-corn-job
+
+go 1.21.6
+
+require github.com/mattn/anko v0.1.9
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/mattn/anko/env"
+	"github.com/mattn/anko/vm"
+)
+
+func main() {
+	e := env.NewEnv()
+
+	err := e.Define("println", fmt.Println)
+	if err != nil {
+		log.Fatalf("Define error: %v\n", err)
+	}
+
+	script := `
+println("Hello World :)")
+`
+
+	_, err = vm.Execute(e, nil, script)
+	if err != nil {
+		log.Fatalf("Execute error: %v\n", err)
+	}
+
+	// output: Hello World :)
+}
